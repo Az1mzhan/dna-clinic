dc601a63cda24407f42cde62847876fe39bb22d9
AzimZen <azimzhaan@gmail.com>
2024-07-21 21:16:57 +0500

    Changed the output directory fot the project build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "public", // Specify the output directory as 'public'
+    outDir: "dist", // Specify the output directory as 'public'
   },
 });
